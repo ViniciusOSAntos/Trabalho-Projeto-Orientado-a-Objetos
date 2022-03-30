e14f9286c4db217263bd84c0071ca761140678fe
Leandro Barros Pontes <65022896+LeandroBPontes@users.noreply.github.com>
2022-03-30 19:23:46 -0300

    Revert "Revert "Estrutura de pastas e inicio da api""
    This reverts commit 41d7f7fe62396cb5301a7dc19f65104c408777cb.

---
+++ Dominios/Usuario.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ControleOrcamento.Models {
+    public class Usuario {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Senha { get; set; }
+    }
+}
---
+++ Repositorios/Contratos/IRepositorioBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ControleOrcamento.Repositorios.Contratos {
+    public interface IRepositorioBase<T, TPrimary> : IDisposable {
+        IEnumerable<T> Get();
+        T GetByID(TPrimary id);
+        void Insert(T model);
+        void Delete(TPrimary id);
+        void Update(T model);
+    }
+}
