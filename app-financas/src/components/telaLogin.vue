e37218757cba03ea6208b034862e87d448e0bcd7
Vinicius Oliveira dos Santos <43036615+ViniciusOSAntos@users.noreply.github.com>
2022-04-19 17:28:09 -0300

    TelaLogin
    Fiz algumas melhorias no design da tela de Login

--- app-financas/src/components/telaLogin.vue
+++ app-financas/src/components/telaLogin.vue
@@ -1,124 +1,144 @@
 <template>
   <div class="sidenav bg-primary">
-         <img alt="Logo Finans" src="../assets/logo.png">
-         <div class="login-main-text">
-            <h2>Finans<br> Controle sua vida financeira!</h2>
-            <p>Acesse sua conta ou registre-se já!</p>
-         </div>
+    <img alt="Logo Finans" src="../assets/logo.png" style="margin-top: 30%" />
+    <div class="login-main-text">
+      <h2>
+        Finans<br />
+        Controle sua vida financeira!
+      </h2>
+      <p>Acesse sua conta ou registre-se já!</p>
+    </div>
+  </div>
+  <div class="main">
+    <div class="col-md-10 col-sm-12">
+      <img
+        src="../assets/moedas.png"
+        class="rounded float-right w-25"
+        alt="finans"
+        style="margin-top: 10%"
+      />
+      <div class="login-form">
+        <form>
+          <div class="form-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              class="form-control"
+              placeholder="Email"
+              v-model.lazy.trim="login.email"
+            />
+          </div>
+          <div class="form-group">
+            <label>Senha:</label>
+            <input
+              type="password"
+              class="form-control"
+              placeholder="Senha"
+              v-model="login.senha"
+            />
+          </div>
+          <br />
+          <button type="submit" class="btn btn-primary">Login</button>
+          <button
+            type="submit"
+            class="btn btn-secondary"
+            style="margin-left: 2px"
+          >
+            Registrar
+          </button>
+          <!-- <button type="submit" class="btn btn-danger" @click.prevent="ver()">Ver</button> -->
+        </form>
       </div>
-      <div class="main">
-         <div class="col-md-6 col-sm-12">
-            <div class="login-form">
-               <form>
-                  <div class="form-group">
-                     <label>Email:</label>
-                     <input type="email" class="form-control" placeholder="Email"
-                     v-model.lazy.trim="login.email">
-                  </div>
-                  <div class="form-group">
-                     <label>Senha:</label>
-                     <input type="password" class="form-control" placeholder="Senha"
-                     v-model='login.senha'>
-                  </div> <br>
-                  <button type="submit" class="btn btn-primary">Login</button>
-                  <button type="submit" class="btn btn-secondary">Registrar</button>
-                  <button type="submit" class="btn btn-danger" @click.prevent="ver()">Ver</button>
-               </form>
-            </div>
-         </div>
-      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'telaLogin',
-    methods: {
-        ver(){
-            console.log('Email: ' + this.login.email + ' Senha: ' + this.login.senha)
-        }
+  name: "telaLogin",
+  methods: {
+    ver() {
+      console.log("Email: " + this.login.email + " Senha: " + this.login.senha);
     },
-    data(){
-        return{
-            login: {
-                email: '',
-                senha: '',
-            }
-        }
-    }
-}
+  },
+  data() {
+    return {
+      login: {
+        email: "",
+        senha: "",
+      },
+    };
+  },
+};
 </script>
 
 <style>
-    body {
-    font-family: "Lato", sans-serif;
+body {
+  font-family: "Lato", sans-serif;
 }
 
-
-
-.main-head{
-    height: 150px;
-    background: #FFF;
-   
+.main-head {
+  height: 150px;
+  background: #fff;
 }
 
 .sidenav {
-    height: 100%;
-    background-color: #000;
-    overflow-x: hidden;
-    padding-top: 20px;
+  height: 100%;
+  background-color: #000;
+  overflow-x: hidden;
+  padding-top: 20px;
 }
 
-
 .main {
-    padding: 0px 10px;
+  padding: 0px 10px;
+  padding-left: 50px;
 }
 
 @media screen and (max-height: 450px) {
-    .sidenav {padding-top: 15px;}
+  .sidenav {
+    padding-top: 15px;
+  }
 }
 
 @media screen and (max-width: 450px) {
-    .login-form{
-        margin-top: 10%;
-    }
+  .login-form {
+    margin-top: 10%;
+  }
 
-    .register-form{
-        margin-top: 10%;
-    }
+  .register-form {
+    margin-top: 10%;
+  }
 }
 
-@media screen and (min-width: 768px){
-    .main{
-        margin-left: 40%; 
-    }
+@media screen and (min-width: 768px) {
+  .main {
+    margin-left: 40%;
+  }
 
-    .sidenav{
-        width: 40%;
-        position: fixed;
-        z-index: 1;
-        top: 0;
-        left: 0;
-    }
+  .sidenav {
+    width: 40%;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+  }
 
-    .login-form{
-        margin-top: 80%;
-    }
+  .login-form {
+    margin-top: 15%;
+  }
 
-    .register-form{
-        margin-top: 20%;
-    }
-}
-
-
-.login-main-text{
+  .register-form {
     margin-top: 20%;
-    padding: 60px;
-    color: #fff;
+  }
 }
 
-.login-main-text h2{
-    font-weight: 300;
+.login-main-text {
+  margin-top: 20%;
+  padding: 60px;
+  color: #fff;
 }
 
-
+.login-main-text h2 {
+  font-weight: 300;
+}
 </style>
