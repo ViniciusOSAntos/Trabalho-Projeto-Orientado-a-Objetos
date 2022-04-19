68ed17ec9d0675a97b05edc8e09d97c5acbee8d4
Vinicius Oliveira dos Santos <43036615+ViniciusOSAntos@users.noreply.github.com>
2022-04-19 11:02:14 -0300

    TelaLogin
    Primeiro protótipo de tela de login para o projeto.

---
+++ app-financas/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <tela-login></tela-login>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import telaLogin from './components/telaLogin.vue'
+
+export default {
+  name: 'App',
+  components: {
+    telaLogin,
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
