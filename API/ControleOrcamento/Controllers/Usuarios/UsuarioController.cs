3cf8b1043f1e96194b7d79a97c44245c5130e8da
Leandro Barros Pontes <65022896+LeandroBPontes@users.noreply.github.com>
2022-04-22 11:09:12 -0300

    migrations e finalizacao

---
+++ API/ControleOrcamento/Controllers/Usuarios/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using ControleOrcamento.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using ControleOrcamento.Models;
+using ControleOrcamento.Repositorios.Contratos;
+
+namespace ControleOrcamento.Controllers.Usuarios {
+    [Route("api/usuario")]
+    [ApiController]
+    public class UsuarioController : CrudBaseController<Usuario, int> {
+
+        // private readonly IRepositorioBase<Usuario, int> _repositorio;
+        public UsuarioController(IRepositorioBase<Usuario, int> repositorio) : base(repositorio) {
+            //  _repositorio = repositorio;
+        }
+    }
+
+}
